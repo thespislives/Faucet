e0d84052cd10a0cd9f5a949f5352e05088161228
Aaron McCoy <thespislives@gmail.com>
2012-03-02 11:17:07 -0500

    Update src/main/java/tk/thespislives/faucet/BlockWriter.java

--- src/main/java/tk/thespislives/faucet/BlockWriter.java
+++ src/main/java/tk/thespislives/faucet/BlockWriter.java
@@ -4,23 +4,7 @@ import java.io.*;
 
 public class BlockWriter {
 
-    public String nameOfMod;
-    public String nameOfBlock;
-    public String blockTexture;
     public String blockMaterial;
-    public String nation;
-    public String userName;
-    public String packageName = nation + "." + userName + "." + nameOfBlock;
-    public String ingredient1;
-    public String ingredient2;
-    public String ingredient3;
-    public String ingredient4;
-    public String ingredient5;
-    public String ingredient6;
-    public String ingredient7;
-    public String ingredient8;
-    public String ingredient9;
-    public String uncraftable;
     public String soundEffect;
     public int friction;
     public int hardness;
@@ -36,11 +20,11 @@ public class BlockWriter {
 
         try {
             // Create file
-            FileWriter BLOCKLIST = new FileWriter(nameOfMod + ".java");
+            FileWriter BLOCKLIST = new FileWriter(BasicWriter.nameOfMod + ".java");
             BufferedWriter out = new BufferedWriter(BLOCKLIST);
             out.append(
                     "package "
-                    + packageName
+                    + BasicWriter.packageName
                     + ";\n"
                     + "\n"
                     + "import org.bukkit.World;\n"
@@ -52,12 +36,12 @@ public class BlockWriter {
                     + "import org.getspout.spoutapi.player.SpoutPlayer;\n"
                     + "\n"
                     + "public class defaultPlugin extends GenericCubeCustomBlock {\n"
-                    + "	public defaultPlugin(Plugin plugin) {\n"
+                    + "    public defaultPlugin(Plugin plugin) {\n"
                     + "		super(plugin, \""
-                    + nameOfBlock
+                    + BasicWriter.nameOfBlock
                     + "\",\n"
                     + "				\""
-                    + blockTexture
+                    + BasicWriter.texture
                     + "\", 16);\n"
                     + "	}\n"
                     + "\n"
@@ -97,11 +81,11 @@ public class BlockWriter {
         }
         try {
             // Create file
-            FileWriter BLOCKLIST = new FileWriter(nameOfBlock + ".java");
+            FileWriter BLOCKLIST = new FileWriter(BasicWriter.nameOfBlock + ".java");
             BufferedWriter out = new BufferedWriter(BLOCKLIST);
             out.write(
                     "package "
-                    + packageName
+                    + BasicWriter.packageName
                     + ";\n"
                     + "\n"
                     + "import java.util.logging.Level;\n"
@@ -114,65 +98,65 @@ public class BlockWriter {
                     + "import org.getspout.spoutapi.material.*;\n"
                     + "\n"
                     + "public class "
-                    + nameOfBlock
+                    + BasicWriter.nameOfBlock
                     + " extends JavaPlugin {"
                     + "\n"
                     + "	public static CustomBlock newBlock;\n"
                     + "\n"
                     + "	public void onDisable() {"
                     + "		Bukkit.getLogger().log(Level.INFO, \""
-                    + nameOfMod
+                    + BasicWriter.nameOfMod
                     + " Disabled!\");\n"
                     + "			}\n"
                     + "	\n"
                     + "	public void onEnable() {"
                     + "		newBlock = new "
-                    + nameOfMod
+                    + BasicWriter.nameOfMod
                     + "(this);\n"
                     + "		newBlock.setFriction(" + friction + ");"
                     + "		newBlock.setHardness(" + hardness + ");"
                     + "		newBlock.setLightLevel(" + lightLevel + ");"
-                    + "		newBlock.setName(" + nameOfBlock + ");"
+                    + "		newBlock.setName(" + BasicWriter.nameOfBlock + ");"
                     + "		newBlock.setOpaque(" + opaque + ");"
                     + "		newBlock.setStepSound(" + soundEffect + ");"
                     + "		Bukkit.getLogger().log(Level.INFO,"
                     + "				\""
-                    + nameOfMod
+                    + BasicWriter.nameOfMod
                     + " by "
-                    + userName
+                    + BasicWriter.userName
                     + " Enabled!\");\n"
                     + "		SpoutItemStack result = new SpoutItemStack("
-                    + nameOfBlock
+                    + BasicWriter.nameOfBlock
                     + ".newBlock, 1);\n"
                     + "		newBlock.setItemDrop(result);"
                     + "		SpoutShapedRecipe recipe = new SpoutShapedRecipe(result);\n"
                     + "		recipe.shape(\"ABC\", \"DEF\", \"GHI\")\n;"
                     + "		recipe.setIngredient('A', MaterialData."
-                    + ingredient1
+                    + BasicWriter.ingredient1
                     + ");\n"
                     + "		recipe.setIngredient('B', MaterialData."
-                    + ingredient2
+                    + BasicWriter.ingredient2
                     + ");\n"
                     + "		recipe.setIngredient('C', MaterialData."
-                    + ingredient3
+                    + BasicWriter.ingredient3
                     + ");\n"
                     + "		recipe.setIngredient('D', MaterialData."
-                    + ingredient4
+                    + BasicWriter.ingredient4
                     + ");\n"
                     + "		recipe.setIngredient('E', MaterialData."
-                    + ingredient5
+                    + BasicWriter.ingredient5
                     + ");\n"
                     + "		recipe.setIngredient('F', MaterialData."
-                    + ingredient6
+                    + BasicWriter.ingredient6
                     + ");\n"
                     + "		recipe.setIngredient('G', MaterialData."
-                    + ingredient7
+                    + BasicWriter.ingredient7
                     + ");\n"
                     + "		recipe.setIngredient('H', MaterialData."
-                    + ingredient8
+                    + BasicWriter.ingredient8
                     + ");\n"
                     + "		recipe.setIngredient('I', MaterialData."
-                    + ingredient9
+                    + BasicWriter.ingredient9
                     + ");\n"
                     + "		SpoutManager.getMaterialManager().registerSpoutRecipe(recipe);"
                     + "	}\n" + "}\n");
@@ -184,10 +168,10 @@ public class BlockWriter {
 
         try {
             //Create folder
-            boolean success = (new File(nameOfMod)).mkdir();
+            boolean success = (new File(BasicWriter.nameOfMod)).mkdir();
             if (success) {
                 System.out.println("Directory: "
-                        + nameOfMod + " created");
+                        + BasicWriter.nameOfMod + " created");
             }
 
         } catch (Exception e) {// Catch exception if any
@@ -196,10 +180,10 @@ public class BlockWriter {
 
         try {
             // File (or directory) to be moved
-            File file1 = new File(nameOfMod + ".java");
-            File file2 = new File(nameOfBlock + ".java");
+            File file1 = new File(BasicWriter.nameOfMod + ".java");
+            File file2 = new File(BasicWriter.nameOfBlock + ".java");
             // Destination directory
-            File dir = new File(nameOfMod);
+            File dir = new File(BasicWriter.nameOfMod);
             // Move file to new directory
             boolean success = file1.renameTo(new File(dir, file1.getName()));
             if (!success) {
